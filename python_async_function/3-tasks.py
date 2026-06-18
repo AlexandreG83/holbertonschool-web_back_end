5c9531f128bb013c59716c5a95be80056378cb02
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-06-18 11:05:32 +0200

    good import; attibut asyncio.create_task to tasks

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -4,21 +4,14 @@ Execute multiple tasks concurrently.
 """
 
 import asyncio
-from typing import List
 
-task_wait_random = __import__('3-tasks').task_wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+def task_wait_n(max_delay: int) -> asyncio.Task:
     """
     Spawn task_wait_random n times and
     return the delays in ascending order.
     """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-
-    delays = []
-
-    for task in asyncio.as_completed(tasks):
-        delays.append(await task)
-
-    return delays
+    tasks = asyncio.create_task(wait_random(max_delay))
+    return my_task
