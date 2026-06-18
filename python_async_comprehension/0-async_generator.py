83444e713bf43db1475481c29010a2422e711762
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-06-18 11:30:37 +0200

    asyncGenerator to Generator; just sleep & uniform

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,15 +3,14 @@
 Create an asynchronous generator.
 """
 
-import asyncio
-import random
-from typing import AsyncGenerator
+from random import uniform
+from typing import Generator
+from asyncio import sleep
 
-
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Generate random numbers asynchronously.
     """
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        await sleep(1)
+        yield uniform(0, 10)
