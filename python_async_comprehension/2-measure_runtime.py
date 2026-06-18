80f19500af305a776a3654d1697f3d02c89aca0e
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-06-18 11:44:01 +0200

    make good iterable in asyncio.gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -18,12 +18,7 @@ async def measure_runtime() -> float:
     """
     start = time.time()
 
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
 
     end = time.time()
 
